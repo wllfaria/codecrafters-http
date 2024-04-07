8609aae6df0253b3d6619a180b0f7781971eb39e
Willians Faria <dev.willians.faria@gmail.com>
2024-04-07 16:51:18 -0300

    sending 404 for not / routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{BufRead, BufReader, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -8,20 +8,33 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let buf_reader = BufReader::new(&mut stream);
-
-                let _request: Vec<_> = buf_reader
-                    .lines()
-                    .map(|l| l.unwrap())
-                    .take_while(|l| !l.is_empty())
-                    .collect();
-
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap()
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let request: Vec<_> = buf_reader
+        .lines()
+        .map(|l| l.unwrap())
+        .take_while(|l| !l.is_empty())
+        .collect();
+
+    if let Some(path) = request
+        .first()
+        .map(|l| l.split_whitespace().skip(1).take(1).collect::<String>())
+    {
+        if path == "/" {
+            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap()
+        } else {
+            stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap()
         }
     }
 }
