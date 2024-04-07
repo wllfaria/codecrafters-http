cd694c0da87560a6323a3074a1f45f99ea0403c1
Willians Faria <dev.willians.faria@gmail.com>
2024-04-07 16:39:08 -0300

    reading request and sending back status 200

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let buf_reader = BufReader::new(&mut stream);
+
+                let _request: Vec<_> = buf_reader
+                    .lines()
+                    .map(|l| l.unwrap())
+                    .take_while(|l| !l.is_empty())
+                    .collect();
+
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
